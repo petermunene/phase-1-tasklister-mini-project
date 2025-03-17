1a2c745ea8fa7077e72b6f93f85ea7fa0361936a
petermunene <peter.munene@student.moringaschool.com>
2025-03-17 21:06:40 +0300

    prevent default

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); 
+
+        const taskInput = document.getElementById("new-task-description");
+        const taskText = taskInput.value; 
+
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.textContent = taskText;
+            taskList.appendChild(li);
+            taskInput.value = ""; 
+        }
+    });
 });
